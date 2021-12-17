cf707046df592f5945d8b74e6219e67324607a35
Rahul-107 <rahul20107@iiitd.ac.in>
2021-12-17 20:16:35 +0530

    Third Comment

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,38 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primarystage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        stage.setTitle("WILL HERO");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("Intro.fxml"));
+        primarystage.setTitle("WILL HERO");
+        primarystage.setResizable(false);
+        primarystage.setX(100);
+        primarystage.setY(0);
+        primarystage.setScene(new Scene(root));
+        primarystage.show();
 
 
-        //Stage stage = new Stage();
-        /*
-        Group root = new Group();
-        Button btn = new Button("Start Game");
-        Scene scene = new Scene(btn, 200,250);
-        Scene scene1 = new Scene(root, Color.LIGHTGREEN);
+        //stage.setScene(new Scene(root));
+        //stage.show();
+
+        //Group root = new Group();
 
         //  Image icon = new Image("icon.png");
         //  stage.getIcons().add(icon);
+        /*
         stage.setTitle("WILL HERO ");
         stage.setWidth(1480);
         stage.setHeight(828);
@@ -40,15 +41,15 @@ public class Main extends Application {
         stage.setY(0);
         stage.setFullScreen(true);
         stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
-
-        stage.setScene(scene);
-        stage.show();
-
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("Esc"));
          */
+
+
 
     }
 
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
