c7e93c21f1cb410ec8e6be5cc130c2d9ee3c083d
Rahul-107 <rahul20107@iiitd.ac.in>
2021-12-17 23:43:00 +0530

    Seventh Comment

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
