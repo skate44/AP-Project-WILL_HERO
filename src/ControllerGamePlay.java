a9bfa3caf13ff36ea2d4927574adb40f8fdd5dd4
Rahul-107 <rahul20107@iiitd.ac.in>
2021-12-19 22:13:46 +0530

    19 Comment

---
+++ src/ControllerGamePlay.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+public class ControllerGamePlay implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image image = new Image(getClass().getResourceAsStream("/Image/set.PNG"));
+        imageView.setImage(image);
+    }
+
+}
--- src/ControllerViewScore.java
+++ src/ControllerViewScore.java
@@ -14,6 +14,7 @@ public class ControllerViewScore {
     private Parent root;
 
     public void BackToMainMenu(ActionEvent event) throws IOException {
+        // Back Button
         root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
