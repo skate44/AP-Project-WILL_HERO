1209f352425ae9a9fcff5728d87f642ca862eb42
Rahul-107 <rahul20107@iiitd.ac.in>
2021-12-19 07:59:05 +0530

    Eleventh Comment

--- src/ControllerMainMenu.java
+++ src/ControllerMainMenu.java
@@ -1,11 +1,20 @@
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerMainMenu implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        Image image = new Image(getClass().getResourceAsStream("/Image/ForMenu.png"));
+        imageView.setImage(image);
     }
 }
