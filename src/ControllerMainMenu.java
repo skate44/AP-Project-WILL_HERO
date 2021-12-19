42db82f402907554ea3a2f73daa2f601151c2072
Rahul-107 <rahul20107@iiitd.ac.in>
2021-12-19 13:53:37 +0530

    15 Comment

--- src/ControllerMainMenu.java
+++ src/ControllerMainMenu.java
@@ -1,14 +1,20 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -42,5 +48,8 @@ public class ControllerMainMenu implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Image image = new Image(getClass().getResourceAsStream("/Image/ForMenu.png"));
         imageView.setImage(image);
+
+
+
     }
 }
